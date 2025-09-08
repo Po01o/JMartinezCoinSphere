04288ff7c6440e7d41fea925cda29cd2fa317861
Po01o <carlosjoel.martinezlopez@gmail.com>
2025-09-08 16:57:22 -0600

    Arreglo de detalles en la App

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -10,17 +10,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.coinsphere.models.coinsList
 import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
@@ -58,19 +64,29 @@ fun coinSphere() {
     Column(
         modifier = Modifier
             .background(Background)
-            .padding(30.dp)
+            .padding(top = 30.dp)
+            .padding(bottom = 30.dp)
             .fillMaxSize()
     ) {
         //CoinSphere
-        Column {
+        Column(
+            modifier = Modifier
+                .padding(start = 30.dp)
+                .padding(end = 30.dp)
+        ) {
             Text(
                 text = "CoinSphere",
-                color = TextMain
+                color = TextMain,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
             )
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 10.dp)
+                    .clip(RoundedCornerShape(15.dp))
                     .background(Surface)
             ) {
                 Column(
@@ -79,11 +95,16 @@ fun coinSphere() {
                 ) {
                     Text(
                         text = "Global Market Cap",
-                        color = TextDim
+                        color = TextDim,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .padding(bottom = 5.dp)
                     )
                     Text(
                         text = "$2.18T",
-                        color = TextMain
+                        color = TextMain,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold
                     )
                 }
             }
@@ -91,6 +112,7 @@ fun coinSphere() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 10.dp)
+                    .clip(RoundedCornerShape(15.dp))
                     .background(Surface)
             ) {
                 Column(
@@ -99,11 +121,16 @@ fun coinSphere() {
                 ) {
                     Text(
                         text = "Fear & Greed",
-                        color = TextDim
+                        color = TextDim,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .padding(bottom = 5.dp)
                     )
                     Text(
                         text = "Neutral (54)",
-                        color = TextMain
+                        color = TextMain,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold
                     )
                 }
             }
@@ -111,6 +138,7 @@ fun coinSphere() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 10.dp)
+                    .clip(RoundedCornerShape(15.dp))
                     .background(Surface)
             ) {
                 Column(
@@ -119,44 +147,70 @@ fun coinSphere() {
                 ) {
                     Text(
                         text = "Altcoin Season",
-                        color = TextDim
+                        color = TextDim,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .padding(bottom = 5.dp)
                     )
                     Text(
                         text = "No",
-                        color = TextMain
+                        color = TextMain,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold
                     )
                 }
             }
         }
 
         //Valores
-        Column {
-            Row {
+        Column{
+            Row(
+                modifier = Modifier
+                    .padding(top = 0.dp)
+                    .padding(bottom = 10.dp)
+                    .padding(start = 40.dp)
+                    .padding(end = 40.dp)
+            ) {
                 Text(
                     text = "#",
                     color= TextDim,
+                    fontSize = 14.sp,
                     modifier = Modifier
                         .weight(1f)
                 )
                 Text(
                     text = "Name",
                     color= TextDim,
+                    fontSize = 14.sp,
                     modifier = Modifier
                         .weight(1f)
                 )
                 Text(
                     text = "Price",
                     color= TextDim,
+                    fontSize = 14.sp,
                     modifier = Modifier
                         .weight(1f)
                 )
             }
-            LazyColumn {
+            Box(
+                modifier = Modifier
+                    .height(1.8.dp)
+                    .fillMaxWidth()
+                    .background(Surface)
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .padding(top = 15.dp)
+                    .padding(start = 30.dp)
+                    .padding(end = 30.dp)
+            ) {
                 items(coinsList) { coin ->
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(bottom = 10.dp)
+                            .clip(RoundedCornerShape(10.dp))
                             .background(Surface)
                     ) {
                         Row(
@@ -166,6 +220,7 @@ fun coinSphere() {
                             Text(
                                 text = coin.number.toString(),
                                 color = TextDim,
+                                fontSize = 13.sp,
                                 modifier = Modifier
                                     .weight(1f)
                             )
@@ -185,8 +240,6 @@ fun coinSphere() {
                         }
                     }
                 }
-
-
             }
         }
     }
